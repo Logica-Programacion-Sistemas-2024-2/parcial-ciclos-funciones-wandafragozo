2405292d16ed45624defe7db431f6785d6467fa9
wandafragozo <wanda.fragozo@upb.edu.co>
2024-08-22 07:51:00 -0500

    parcial 1

--- app/src/main/java/parcial1g1/App.java
+++ app/src/main/java/parcial1g1/App.java
@@ -1,11 +1,69 @@
 
-package parcial1g1;
+
+package parcial.wanda;
+
+import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
-        
+    double kilometro1 = 0;
+    double metrosVehiculo2 = 0;
+    double metrosVehiculo3 = 0;
+    int cantidadVehiculos = 0;
+    double resultado1 = 0;
+    double litrosGasolina1 = 0;
+    double kilometro2 = 0;
+    double litrogasolina2 = 0;
+    double kilometro3 = 0;
+
+    
+   
+
+
+    public static void main(String[] args) {  
         // Implementar la logica de solucion
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese la cantidad de vehiculos: ");
+        int cantidadVehiculos = sc.nextInt();
+
+        try {
+            if (cantidadVehiculos > 0) {
+                Scanner metroScanner = new Scanner(System.in);
+                System.out.println("ingrese los metros recorridos por el vehiculo 1: ");
+                double metroVehiculo1 = metroScanner.nextInt();
+
+                kilometro1 = (metroVehiculo1 / 1000);
+                litrosGasolina1 = (kilometro1 * 55)/750;
+                
+
+                resultado1 = kilometro1 + litrosGasolina;
+
+
+                
+            }else if (metrosVehiculo2 > 0) {
+                System.out.println("ingrese los mestros recorridos por el vehiculo 2: ");
+                double metrosVehiculo2 = metroScanner.nextInt();
+
+                kilometro2 = (metrosVehiculo2 / 1000);
+                litrogasolina2 = (kilometro2 * 55)/750;
+                   
+            }else if (metrosVehiculo3 > 0 ) {
+                System.out.println("ingrese la cantidad de metros recorridos por el vehiculo 3: ");
+                double metrosVehiculo3 = metroScanner.nextInt();
+
+                kilometro2 = (metrosVehiculo3 / 1000);
+                litrogasolina2 = (kilometro3 * 55)/750;
+
+
+                
+            }else {
+
+            }
+            
+        } catch (Exception e) {
+            
+        }
         
     }
 
@@ -17,8 +75,18 @@ public class App {
     */
     //------------------------------------------------------------------
     //Implemente la funcion en este espacio
-    
+    public static float calcularConsumoGasolina(float kmRecorrido){
 
+       int  GaslinaTotal = litrosGasolina1 + litrogasolina2 + 
+
+        
+
+
+
+
+
+        return ((kmRecorrido * 55) / 750);
+    }
     //------------------------------------------------------------------
 
 }
